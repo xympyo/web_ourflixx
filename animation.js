1ebc55651fad5338123de083f7398da5142ccafc
Moshe Dayan <xynpyro@gmail.com>
2024-07-21 18:15:15 +0700

    commit lol
    ye

---
+++ animation.js
@@ -0,0 +1,10 @@
+function addReveal() {
+  var header = document.getElementById("header");
+  var hero = document.getElementById("hero");
+  var images = document.getElementById("image");
+  var hero_below = document.getElementById("hero_below");
+  header.classList.add("reveal");
+  hero.classList.add("reveal");
+  images.classList.add("reveal");
+  hero_below.classList.add("reveal");
+}
